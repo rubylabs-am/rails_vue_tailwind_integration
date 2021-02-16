1d83313eecf22177008540aa837b3d530b004f13
Meliq Pilosyan <melopilosyan@gmail.com>
2021-02-16 07:22:37 +0400

    Add pages controller with vue components in view files: autodetected components

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def index; end
+
+  def test
+    @messages = %w[Testing\ vue\ world Next\ line And\ one\ more\ here]
+  end
+
+  def another_test; end
+end
