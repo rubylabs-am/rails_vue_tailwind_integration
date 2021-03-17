46d151acfb4eb3d7a7378fb85061b5c6c4038075
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-03-17 20:13:03 +0400

    Refs #1/added nested component link_to.vue

---
+++ app/javascript/src/components/nested/link_to.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :href="url" class="mr-4 py-0 px-5
+          ring-offset-2 hover:ring-1 hover:ring-indigo-500
+          focus:ring-2 focus:ring-indigo-400 focus:outline-none
+          bg-gradient-to-r from-yellow-100 to-green-200
+          text-gray-500 rounded-lg shadow"
+  >
+    <slot>Link</slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'v-link-to',
+
+  props: {
+    url: {
+      type: String,
+      default: ''
+    }
+  }
+}
+
+</script>
