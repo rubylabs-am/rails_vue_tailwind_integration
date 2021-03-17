9ef560a649ed58305853cdd319daf617e7945fe7
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-03-17 20:01:59 +0400

    Refs #1/in controllers TodoListsController and TodoItemsController initialized their methods and added methods for params....

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,13 +1,51 @@
 class TodoItemsController < ApplicationController
-  def new
+  skip_before_action :verify_authenticity_token
+  before_action :set_todo_list, only: [:index, :create]
+  before_action :set_todo_item, only: [:edit, :update, :destroy]
+
+  def index
+    @todo_items =  @todo_list.todo_items
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @todo_items  }
+    end
   end
 
   def create
+    @todo_item = @todo_list.todo_items.build todo_item_params
+    if @todo_item.save
+      render json: { todo_item: @todo_item }
+    else
+      render json: { success: false }
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
   def update
+  end
+
+  def destroy
+    @todo_item.destroy
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find_by id: params[:todo_list_id]
+  end
+
+  def set_todo_item
+    @todo_list = set_todo_list
+    @todo_item = @todo_list.todo_items.find_by id: params[:id]
+  end
+
+  def todo_item_params
+    params.require(:todo_item).permit :name, :details
   end
 end
--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,16 +1,43 @@
 class TodoListsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_todo_list, only: [:edit, :update, :destroy]
+
   def index
+    respond_to do |format|
+      format.html
+      format.json { render json: TodoList.all }
+    end
   end
 
   def new
   end
 
   def create
+    @todo_list = TodoList.new todo_list_params
+    if @todo_list.save
+      render json: { todo_list: @todo_list }
+    else
+      render json: { success: false }
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def destroy
+    @todo_list.destroy
+  end
+
+  private
+
+  def todo_list_params
+    params.require(:todo_list).permit :name
+  end
+
+  def set_todo_list
+    @todo_list = TodoList.find_by id: params[:id]
   end
 end
